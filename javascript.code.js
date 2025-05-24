96b1bd4e756bb746e7c2d54a8a1d2087f1d4fbe9
Saiharshith Eedara <142712608+Saiharshith17@users.noreply.github.com>
2025-05-24 21:42:39 +0530

    Update javascript.code.js

--- javascript.code.js
+++ javascript.code.js
@@ -4,6 +4,7 @@ var states=new Array("a","b","c");     //this is the second way of writing an ar
 console.log(states);
 console.log(states.length);
 
+
 console.log(states[0]);
 console.log(states.indexOf("a"));
 states[0] = "z";
