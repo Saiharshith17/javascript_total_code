fd4cf0eea91cc3ac4a012d4c6e2508f701126e20
Saiharshith Eedara <142712608+Saiharshith17@users.noreply.github.com>
2025-05-25 21:38:57 +0530

    Update javascript.code.js

--- javascript.code.js
+++ javascript.code.js
@@ -3,7 +3,6 @@ console.log(countrieas);
 var states=new Array("a","b","c");     //this is the second way of writing an array
 console.log(states);
 console.log(states.length);
-
 console.log(states[0]);
 console.log(states.indexOf("a"));
 states[0] = "z";
