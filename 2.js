560f3477a3279ae678e76635ec45c5123cc6f5e3
Saiharshith17 <eedarasaiharshith6699@gmail.com>
2025-08-11 10:32:45 +0530

    added

--- 2.js
+++ 2.js
@@ -1,3 +1,38 @@
-let {a,username}=require('./1');
-console.log(a);
-console.log(username);
+class Car{
+    constructor(color,speed){
+        this.color=color;
+        this.speed=speed;
+        
+    }
+    drive(){
+        console.log(`Driving the ${this.color} car at speed of ${this.speed} km/hr`);
+    }
+}
+const myCar=new Car('blue',100);
+myCar.drive();
+
+
+class BankAccount{
+    #balance=0;
+
+    deposit(amount){
+        if(amount>0){
+            this.#balance+=amount;
+            console.log(`Deposited ${this.amount} New balance ${this.#balance}`);
+        }
+    }
+
+    withdraw(amount){
+        if(this.#balance>=amount){
+            this.#balance-=amount;
+            console.log(`Withdrew: ₹${amount}. New balance: ₹${this.#balance}`);
+
+        }else{
+            console.log(`Insufficient funds`);
+            return;
+        }
+    }
+}
+const mine=new BankAccount();
+mine.deposit(5000);
+mine.withdraw(2000);
